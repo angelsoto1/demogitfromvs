08971dc84985bd5fdb0d27209361c4cec257983e
angel <acania29@outlook.es>
2018-08-30 11:23:28 -0600

    clase saludo se creo la clase saludo con una propiedad Mensaje

---
+++ demogitfromvs/saludo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demogitfromvs
+{
+    class saludo
+    {
+        public string Mensaje { get; set; }
+
+        public saludo()
+        {
+            Mensaje = "hola desde visual estudio";
+        }
+    }
+}
